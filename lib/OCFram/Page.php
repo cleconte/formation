c643d3b4b101143068b33d54a1a075be15d88f9e
shibiro <lecomtedecamille@gmail.com>
2015-09-08 12:04:52 +0200

    Front

--- lib/OCFram/Page.php
+++ lib/OCFram/Page.php
@@ -22,25 +22,27 @@ class Page extends ApplicationComponent
     {
       throw new \RuntimeException('La vue spécifiée n\'existe pas');
     }
- 
+
+    $user = $this->app->user();
+
     extract($this->vars);
- 
+
     ob_start();
       require $this->contentFile;
     $content = ob_get_clean();
- 
+
     ob_start();
       require __DIR__.'/../../App/'.$this->app->name().'/Templates/layout.php';
     return ob_get_clean();
   }
- 
+
   public function setContentFile($contentFile)
   {
     if (!is_string($contentFile) || empty($contentFile))
     {
       throw new \InvalidArgumentException('La vue spécifiée est invalide');
     }
- 
+
     $this->contentFile = $contentFile;
   }
 }
